c0aaf4995ced00cc9bad3be46d630414a943d4e1
vonzhou <vonzhou@163.com>
2014-11-07 14:44:43 +0800

    返回静态页面内容

--- README.txt
+++ README.txt
@@ -2,4 +2,12 @@
 Motivated by the AOSA - 500 lines or less , I want to learn from the code 
 and rewrite the web server in java , also learn Jetty deeply.
 
+1.helloweb 
+	简单的响应一个msg
+2.echorequestinfo
+	从请求消息中获得一定的对端信息
+3.servstatic
+	返回静态页面的内容
+4.
+
 -----vonzhou 
--- src/aosa500lines/servestatic/WebServer.java
+++ src/aosa500lines/servestatic/WebServer.java
@@ -1,10 +1,12 @@
 package aosa500lines.servestatic;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
@@ -14,28 +16,46 @@ class RequestHandler extends AbstractHandler{
 
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request,
-			HttpServletResponse reponse) throws IOException, ServletException {
+			HttpServletResponse response) throws IOException, ServletException {
 		// current work directory
 		String base = System.getProperty("user.dir");
+		// 系统默认的文件分隔符,注意路径分隔符是 path.separator
+		String seperator = System.getProperty("file.separator");
 		String requestPath = request.getPathInfo();
-		int index = requestPath.indexOf('/');
 		
-		requestPath = requestPath.substring(index);
-		System.out.println(requestPath);
-		/*
-		File currentDirectory = new File(".");
-		System.out.println(currentDirectory.getCanonicalPath());
-		*/ // 和上面得到的结果一样 E:\GitHub\MyProject\webserver
+		/* Chrome 浏览器存在这个问题: 请求 /favicon.ico ，所以特殊处理
+		 * 按照 RFC 2616 至少应该返回 
+		 * HTTP/1.0 404 <CRLF>
+		 * <CRLF>
+		 */
+		if(requestPath.equals("/favicon.ico")){
+			//TODO
+			return ;
+		}
+			
+		// 获得绝对路径，判断是否存在
+		String absolueteRequestPath =(base +  request.getPathInfo()).replace("/", seperator);
+		//判断这个路径是否是一个文件
+		File path = new File(absolueteRequestPath);
+		String res = "";
+		String errorPage = "<html><body><h1>Error Accessing "+ requestPath +
+							"</h1></body></html>";
+		if(!path.isFile()){
+			res = errorPage;
+		}else{
+			// 读取文件内容
+			res = TextFile.read(path);
+		}
 		
-		reponse.setContentType("text/html;charset=utf-8");
-		reponse.setStatus(HttpServletResponse.SC_OK);
+		
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.setContentLength(res.length());
 		baseRequest.setHandled(true);
 		
-		reponse.getWriter().println("<h1><font color=red>"+requestPath+"<font></h1>");
-		reponse.getWriter().println("<h1><font color=red>"+base+"<font></h1>");
+		response.getWriter().println(res);
 		
 	}
-	
 }
 
 public class WebServer {
